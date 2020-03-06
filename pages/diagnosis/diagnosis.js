162c1d027df518b2046eb62eac8275745ff13a66
IcyLeaves <leavescy@126.com>
2020-03-06 15:14:52 +0800

    创建辅助诊断/问诊信息 - 辅助诊断切换tab时会回到顶部 - 辅助诊断/问诊信息：UI制作完成 - 辅助诊断/个人信息：全表灰色，提示可以去个人设置里填写，只供确认用

--- pages/diagnosis/diagnosis.js
+++ pages/diagnosis/diagnosis.js
@@ -1,13 +1,4 @@
-var myValidate = require('../../utils/info-form-validate.js')
 var staticData = require('../../utils/static-data.js')
-const validateId = function (rule, value, param, models) {
-  if (typeof (value) == undefined || value == null || value == "") return "身份证必填";
-  if (!myValidate.checkIDCard(value)) {
-    return "身份证格式错误";
-  } else {
-    return null;
-  }
-}
 
 var appInst = getApp();
 
@@ -17,6 +8,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    topNum:-1,
+
     navbar: ['个人信息', '问诊信息', '检查报告', '报告查看'],
     currentTab: 0,
 
@@ -27,75 +20,55 @@ Page({
     marriage: ["未婚", "已婚"],
 
     formData: {},
-    rules: [{
-      name: 'name',
-      rules: {
-        required: true,
-        message: '姓名必填'
+
+
+    diabeteType:["1型糖尿病","2型糖尿病","不清楚"],
+    checkboxItems: [{
+        name: '多饮多尿',
+        value: '0'
       },
-    }, {
-      name: 'id',
-      rules: {
-        validator: validateId
+      {
+        name: '多食',
+        value: '1'
       },
-    }, {
-      name: 'careId',
-      rules: {
-        required: true,
-        message: '医疗卡号必填'
+      {
+        name: '消瘦',
+        value: '2'
       },
-    }, {
-      name: 'sex',
-      rules: {}
-    }, {
-      name: 'age',
-      rules: {}
-    }, {
-      name: 'tel',
-      rules: [{
-        required: true,
-        message: '联系电话必填'
-      }, {
-        mobile: true,
-        message: '联系电话格式错误'
-      }]
-    }, {
-      name: 'emergencyTel',
-      rules: [{
-        required: true,
-        message: '紧急联系方式必填'
-      }, {
-        mobile: true,
-        message: '紧急联系方式格式错误'
-      }]
-    }, {
-      name: 'emergencyName',
-      rules: {}
-    }, {
-      name: 'region',
-      rules: {}
-    }, {
-      name: 'nationIndex',
-      rules: {}
-    }, {
-      name: 'date',
-      rules: {}
-    }, {
-      name: 'familyDiseaseIndex',
-      rules: {}
-    }, {
-      name: 'marriageIndex',
-      rules: {}
-    }, {
-      name: 'address',
-      rules: {}
-    }],
-    // 1.是否填了身份证号，填完后马上校验，校验成功则显示出生日期和性别和年龄
-    isIdCorrect: false
+      {
+        name: '肥胖',
+        value: '3'
+      },
+      {
+        name: '疲乏无力',
+        value: '4'
+      }
+    ],
+    formData1:{diabeteTypeIndex:0}
   },
   // Page展示前调用
   onReady: function (options) {
-    var isCorrect = myValidate.checkIDCard(appInst.globalData.userTable['id']);
+    if (appInst.globalData.userTable['age'] <= 0) {
+      wx.showModal({
+        title: '提示',
+        content: '进行辅助诊断需要完善个人信息，是否现在去完善？',
+        confirmText: '现在就去',
+        cancelText: '暂不完善',
+        success(res) {
+          if (res.confirm) {
+            console.log('用户点击确定');
+            wx.redirectTo({
+              url: '/pages/my/info'
+            })
+          } else if (res.cancel) {
+            console.log('用户点击取消');
+            wx.navigateBack({
+              delta: 1
+            })
+          }
+        }
+      })
+    }
     //提取表单数据
     this.setData({
       ['formData.name']: appInst.globalData.userTable['name'],
@@ -112,32 +85,15 @@ Page({
       ['formData.date']: appInst.globalData.userTable['birthday'],
       ['formData.familyDiseaseIndex']: appInst.globalData.userTable['familyDiseaseIndex'],
       ['formData.marriageIndex']: appInst.globalData.userTable['marriageIndex'],
-      isIdCorrect: isCorrect
     })
   },
-  formInputChange: function (e) {
+  form1InputChange: function (e) {
     const {
       field
     } = e.currentTarget.dataset
     this.setData({
-      [`formData.${field}`]: e.detail.value
+      [`formData1.${field}`]: e.detail.value
     })
-  },
-  //身份证号码改变时且校验成功，则设置年龄，出生日期，性别
-  idInputChange: function (e) {
-    this.formInputChange(e);
-    if (myValidate.checkIDCard(e.detail.value)) {
-      this.setData({
-        [`formData.sex`]: myValidate.getSex(e.detail.value),
-        [`formData.age`]: myValidate.getAge(e.detail.value),
-        [`formData.date`]: myValidate.getDate(e.detail.value),
-        isIdCorrect: true
-      })
-    } else {
-      this.setData({
-        isIdCorrect: false
-      })
-    }
   },
   submitForm() {
     this.selectComponent('#form').validate((valid, errors) => {
@@ -150,21 +106,58 @@ Page({
           })
         }
       } else {
-        wx.showToast({
-          title: '保存成功'
-        });
         this.setData({
-          currentTab: 1
+          currentTab: 1,
+          topNum:0
         });
       }
     })
+  },
+  submitForm1() {
+    this.selectComponent('#form1').validate((valid, errors) => {
+      console.log('valid', valid, errors)
+      if (!valid) {
+        const firstError = Object.keys(errors)
+        if (firstError.length) {
+          this.setData({
+            error: errors[firstError[0]].message
+          })
+        }
+      } else {
+        this.setData({
+          currentTab: 2,
+          topNum:0
+        });
+      }
+    })
+  },
+  checkboxChange: function (e) {
+    var checkboxItems = this.data.checkboxItems,
+      values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems,
+      [`formData1.feature`]: e.detail.value
+    });
   },
   //点击切换，滑块index赋值
   navbarSwitch: function (e) {
     if (this.data.currentTab >= e.currentTarget.dataset.idx)
       this.setData({
-        currentTab: e.currentTarget.dataset.idx
+        currentTab: e.currentTarget.dataset.idx,
+        topNum:0
       })
+
     //全局变量
     //app.globalData.currentTab = e.currentTarget.dataset.idx;
   },
